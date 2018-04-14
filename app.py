051f619ca2e93f7cc24056aa64d7618e04a91347
Antonio <art3kr@virginia.edu>
2018-04-13 20:37:10 -0400

    hi

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ def webhook():
 
   if message_data['name'] != 'newdick':
     if message_data['text'] == 'Dickbot':
-      msg = 'Hello! {}'.format(message_data['name'])
+      msg = 'Hello, {}!'.format(message_data['name'])
       post_message(msg)
     if message_data['text'] == '!info':
       msg = 'Created by Antonio Trani. Build 4/13/18' 
+      post_message(msg)
 
   return "nice", 200
 
